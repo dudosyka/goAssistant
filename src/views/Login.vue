f072e2003e979265bc4bfa47fcb50c4bbd8db69a
Mihail <mohonovproduction@yandex.ru>
2021-04-30 18:13:44 +0300

    auth is work

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="login">
-    <h1>Login Page</h1>
-  </div>
+    <main class="menu">
+      <article class="form_window">
+        <header class="form_title">
+          <button v-on:click="lever = 0">Войти</button> \ <button v-on:click="lever = 1">Зарегистрироваться</button>
+        </header>
+        <section class="form_login" v-show="lever">
+          <input type="mail" class="input" placeholder="Email">
+          <input type="text" class="input" placeholder="Password">
+        </section>
+        <section class="form_signup" v-show="!lever">
+          <input type="text" class="input" placeholder="Email">
+          <input type="text" class="input" placeholder="Nickname">
+        </section>
+      </article>
+    </main>
 </template>
 <script>
   import axios from "axios"
@@ -10,6 +22,10 @@
     name: 'Login',
     data() {
       return {
+        email: null,
+        pass: null,
+        nick: null,
+        lever: 0
       }
     },
     components: {
