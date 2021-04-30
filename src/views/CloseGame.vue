21d77b5954609661bd7b9e082f8ce4883a4415db
Mihail <mohonovproduction@yandex.ru>
2021-04-30 20:20:57 +0300

    animation for back button

--- src/views/CloseGame.vue
+++ src/views/CloseGame.vue
@@ -5,7 +5,7 @@
       <div class="">
         <button class="button-light w3-button w3-card-4 tr w3-hover-white">Создать</button>
         <button class="button-light w3-button w3-card-4 tr w3-hover-white">Присоединиться</button>
-        <router-link class="button-light w3-button w3-card-4 tr w3-hover-white" to="/menu">⟵ Назад</router-link>
+        <router-link class="button-light w3-button w3-card-4 tr w3-hover-white back-button" to="/menu">Назад</router-link>
       </div>
     </article>
   </main>
