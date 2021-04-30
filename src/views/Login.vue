a2d7468058bcfc19ec20e48c937e88229578595d
dudosyka <a.hatson22@gmail.com>
2021-04-30 20:35:46 +0300

    Axios fix + gameField.
    Co-Authored-By: Санёк <dudosyka@users.noreply.github.com>

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -10,12 +10,36 @@
     name: 'Login',
     data() {
       return {
+        email: null,
+        password: null,
       }
     },
     components: {
 
     },
     methods: {
+        auth() {
+
+        let bodyFormData = new FormData();
+
+        bodyFormData.append('email', 'a.hatson@ya.ru');
+        bodyFormData.append('nickname', 'dudosyka');
+
+        axios({
+          method: "post",
+          url: "https://go-backend-denis.ambersoft.llc/user/register",
+          data: bodyFormData,
+          headers: { "Content-Type": "multipart/form-data" },
+        }).then(data => { console.log(data); }).catch(err => { console.log(err); });
+        },
+        statistic() {
+            axios.get('https://go-backend-denis.ambersoft.llc/user/statistics')
+            .then(data => {console.log(data)}).catch(err => {console.log(err)});
+        }
     },
+    created() {
+        this.statistic();
+        this.auth();
+    }
   }
 </script>
