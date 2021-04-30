8fbb4bba2eaa738dcda20d3b2a08acffe31ac0a3
TheMixaDev <bms2004@bk.ru>
2021-04-30 19:48:59 +0300

    +w3css

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -2,11 +2,10 @@
   <main class="menu">
     <article class="form_window menu">
       <div class="">
-        <button class="button-light">Продолжить игру</button>
-        <button class="button-light">Игра со случайным сопреником</button>
-        <button class="button-light">Игра с ИИ</button>
-        <button class="button-light">Закрытая игра</button>
-        <button class="button-light">Рейтинг игроков</button>
+        <button class="button-light w3-button w3-card-4 tr w3-hover-white">Продолжить игру</button>
+        <button class="button-light w3-button w3-card-4 tr w3-hover-white">Игра со случайным соперником</button>
+        <button class="button-light w3-button w3-card-4 tr w3-hover-white">Игра с ИИ</button>
+        <button class="button-light w3-button w3-card-4 tr w3-hover-white">Закрытая игра</button>
       </div>
     </article>
   </main>
