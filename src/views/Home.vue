ee157a937997d519185d78aa2d3a4eaa2cf4f0ed
Mihail <mohonovproduction@yandex.ru>
2021-05-01 16:18:11 +0300

    route to passport

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,17 +5,47 @@
         <button @click='continueGame()' class="button full-width w3-button w3-card-4 tr w3-hover-white point" v-if="alreadyStart">Продолжить игру</button>
         <button @click='randomGame()' class="button full-width w3-button w3-card-4 tr w3-hover-white hi" :disabled="alreadyStart">Игра со случайным соперником</button>
         <button @click='startGameWithBot()' class="button full-width w3-button w3-card-4 tr w3-hover-white lamp" :disabled="alreadyStart">Игра с ИИ</button>
-        <router-link class="button full-width w3-button w3-card-4 tr w3-hover-white looker" to="closegame" tag="button" :disabled="alreadyStart">Частная игра</router-link>
+        <router-link class="button full-width w3-button w3-card-4 tr w3-hover-white loocker" to="/closegame" tag="button" :disabled="alreadyStart">Частная игра</router-link>
       </div>
     </article>
+    <router-link to="/passport" class="bookmarck tr">
+      <img v-bind:src="ava">
+      <span>{{nick}}</span>
+    </router-link>
   </main>
 </template>
+
+<style scoped>
+  .bookmarck {
+    transform: translateX(-120px) rotate(90deg);
+    position: fixed;
+    left: 0;
+    padding: 10px 30px 50px 30px;
+    background: #1f1233;
+    border-radius: 5px;
+  }
+  .bookmarck:hover {
+    transform: translateX(-80px) rotate(90deg) scale(1.05);
+  }
+  .bookmarck img {
+    width: 50px;
+    border-radius: 50%;
+    margin-right: 10px;
+  }
+  .bookmarck span {
+    color: #fff;
+    font-size: 20px;
+  }
+</style>
+
 <script>
   export default {
     name: 'Menu',
     data() {
       return {
           alreadyStart: false,
+          ava: 'https://www.castorama.ru/media/catalog/product/cache/image/1800x/040ec09b1e35df139433887a97daa66f/2/e/2ef250_510229_1.jpg',
+          nick: 'ex@mple'
       }
     },
     components: {
