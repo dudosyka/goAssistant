344d54afe3118c7e345e3cef89ad350d428df9e2
Mihail <mohonovproduction@yandex.ru>
2021-05-01 20:16:08 +0300

    modal window

--- src/views/JoinToCloseGame.vue
+++ src/views/JoinToCloseGame.vue
@@ -8,6 +8,17 @@
         <router-link class="button full-width w3-button w3-card-4 tr w3-hover-white back-button" to="/closegame">Назад</router-link>
       </div>
     </article>
+    <transition name="top-fade">
+      <div class="modal_bg" v-show="modal">
+        <div class="modal">
+          <header>
+              <h1 class="modal-title">Lorem ipsum</h1><span></span>
+              <button class="modal-close" @click="modal = false">❌</button>
+          </header>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+    </transition>
   </main>
 </template>
 
@@ -16,7 +27,8 @@
     name: 'JoinToCloseGame',
     data() {
       return {
-        code: ""
+        code: "",
+        modal: true,
       }
     },
     components: {
